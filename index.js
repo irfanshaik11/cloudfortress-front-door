b01ee82528549e0193f69b5b3c75f37e056e4488
irfan shaik <irfanshaik11@gmail.com>
2023-06-04 05:40:47 -0700

    irfan

--- index.js
+++ index.js
@@ -1,12 +1,74 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
+const crypto = require('crypto');
+const https = require('https')
 
 app.get('/', (request, response) => {
-    response.send(`
-        <h1>Status Code: ${response.statusCode}</h1>
-        <h2>Hello World</h2>
-    `)
+	const token = "c6d8001cb9de02cfe29709f825f32d335f9d63cd6dee69ca304675efb2e5c66a50376c1d94afef4a0b0b6ddf3505939a";
+	const secret = "005efa212c2f76f6ed9aa4ad4523f9e8";
+	const t = Date.now();
+	const nonce = "requestID";
+	const data = token + t + nonce;
+	const signTerm = crypto.createHmac('sha256', secret)
+	    .update(Buffer.from(data, 'utf-8'))
+	    .digest();
+	const sign = signTerm.toString("base64");
+	console.log(sign);
+
+	const body = JSON.stringify({
+	    "command": "turnOn",
+	    "parameter": "default",
+	    "commandType": "command"
+	});
+	const deviceId = "D6254F3CD567";
+	const options = {
+	    hostname: 'api.switch-bot.com',
+	    port: 443,
+	    path: `/v1.1/devices/${deviceId}/commands`,
+	    method: 'POST',
+	    headers: {
+	        "Authorization": token,
+	        "sign": sign,
+	        "nonce": nonce,
+	        "t": t,
+	        'Content-Type': 'application/json',
+	        'Content-Length': body.length,
+	    },
+	};
+
+	// FETCH DEVICE IDS
+	// const options = {
+	//     hostname: 'api.switch-bot.com',
+	//     port: 443,
+	//     path: `/v1.1/devices`,
+	//     method: 'GET',
+	//     headers: {
+	//         "Authorization": token,
+	//         "sign": sign,
+	//         "nonce": nonce,
+	//         "t": t,
+	//         'Content-Type': 'application/json',
+	//         'Content-Length': body.length,
+	//     },
+	// };
+
+
+	const req = https.request(options, res => {
+	    console.log(`statusCode: ${res.statusCode}`);
+	    res.on('data', d => {
+	        process.stdout.write(d);
+	    });
+	});
+
+	req.on('error', error => {
+	    console.error(error);
+	});
+
+	req.write(body);
+	req.end();
+
+    response.send(`<h2>Opened The Door!</h2>`)
 });
 
 app.listen(PORT, () => {
